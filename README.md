5abf4129b0711b82372f061fe086d59fba1af883
AdrianoI15U <adrianordp@alu.ufc.br>
2020-04-02 11:16:40 -0300

    Hello world!

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # README
  This is a reproduction of "Tkinter Course - Create Graphic User Interfaces in Python Tutorial"
+## Lessons so far:
+1. Hello world
---
+++ hello.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+from Tkinter import *
+
+root = Tk()
+
+# Creating a Label Widget
+myLabel = Label(root, text="Hello World!")
+# Shoving it onto the screen
+myLabel.pack()
+
+
+root.mainloop()
